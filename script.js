271f71e5a0b8f83143dcb1c978778efda8ea9eb2
“Thomas-Kenny24” <dice2310@gmail.com>
2022-03-01 09:53:19 -0500

    completed exercise

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log(55);
+
+/*
+Strings
+Boolean
+Numbers
+Null
+Undefined
+*/
+
+/*
++
+-
+*
+/
+%
+**
+*/
+
+console.log(5 % 4);
+console.log(5 % 2);
+
+console.log('I\'M MARRY POPPINS, Y\'ALL!');
+
+console.log("Hawk" + "eye");
+
+console.log("Thomas".length + "Kenny".length);
+
+console.log("Avengers: Endgame"[16]);
